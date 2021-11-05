425037da25377405d68ca4d2242ce1c8f81ca10a
Alex <alex.taylor.ak@gmail.com>
2021-11-05 04:21:43 +0200

    initial

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,60 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  var cvaElement = document.getElementById("cv");
-
-  cvaElement.classList.add("active-style");
-
-  var cvaElement = document.getElementById("cv-icon");
-
-  cvaElement.classList.add("active-icon");
+document.addEventListener("DOMContentLoaded", function (event) {
+  ShowCV();
 });
+
+function ShowCV() {
+  HideDocuments();
+  var aElm = document.getElementById("cv");
+
+  var iElm = document.getElementById("cv-icon");
+
+  var divElm = document.getElementById("cv-content");
+
+  aElm.classList.add("active-style");
+
+  iElm.classList.add("active-icon");
+
+  divElm.classList.remove("hidden-content");
+
+  document.getElementById("page-title").innerHTML = "Curriculum Vitae";
+}
+
+function HideDocuments() {
+  var aElm = document.getElementById("documents");
+
+  var iElm = document.getElementById("documents-icon");
+
+  var divElm = document.getElementById("docs-content");
+
+  divElm.classList.add("hidden-content");
+  aElm.classList.remove("active-style");
+  iElm.classList.remove("active-icon");
+}
+
+function HideCV() {
+  var aElm = document.getElementById("cv");
+
+  var iElm = document.getElementById("cv-icon");
+
+  var divElm = document.getElementById("cv-content");
+
+  divElm.classList.add("hidden-content");
+
+  aElm.classList.remove("active-style");
+
+  iElm.classList.remove("active-icon");
+}
+
+function ShowDocuments() {
+  HideCV();
+  document.getElementById("page-title").innerHTML = "Documents";
+  var aElm = document.getElementById("documents");
+
+  var iElm = document.getElementById("documents-icon");
+
+  var divElm = document.getElementById("docs-content");
+
+  divElm.classList.remove("hidden-content");
+  aElm.classList.add("active-style");
+  iElm.classList.add("active-icon");
+}
