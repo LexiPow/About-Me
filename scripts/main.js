a63897008cd96f358c84f92913dbf94361a6dde9
Alex <alex.taylor.ak@gmail.com>
2021-11-05 02:46:29 +0200

    initial

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var cvaElement = document.getElementById("cv");
+
+  cvaElement.classList.add("active-style");
+
+  var cvaElement = document.getElementById("cv-icon");
+
+  cvaElement.classList.add("active-icon");
+});
